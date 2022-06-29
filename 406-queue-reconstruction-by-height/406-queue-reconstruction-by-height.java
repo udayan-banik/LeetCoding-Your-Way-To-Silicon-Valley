01ab264f9c2208cd5cc0ec44da1b7aed95288b0f
Udayan Banik <banikuday2708@gmail.com>
2022-06-30 01:02:16 +0530

    Time: 5 ms (100.00%), Space: 43.6 MB (81.89%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
@@ -1,31 +1,10 @@
 class Solution {
-    public int[][] reconstructQueue(int[][] people) 
-    {
-        int n=people.length;
-        int index[]=new int[n];
-        Arrays.sort(people,(int a[],int b[])->(a[0]-b[0]==0?b[1]-a[1]:a[0]-b[0]));
-        Arrays.fill(index,-1);
-        for(int i=0;i<n;i++)
-        {
-            int insert_ind=people[i][1];
-            int c=0;
-            for(int j=0;j<n;j++)
-            {
-                if(index[j]==-1)
-                    insert_ind--;
-                if(insert_ind<0)
-                {
-                    index[j]=i;
-                    break;
-                }
-            }
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (o1, o2) -> o1[0] != o2[0] ? o2[0] - o1[0] : o1[1] - o2[1]);
+        List<int[]> tempRes = new ArrayList<>();
+        for (int[] person : people) {
+            tempRes.add(person[1], person);
         }
-        int updated_arr[][]=new int[n][2];
-        for(int i=0;i<n;i++)
-        {
-            updated_arr[i][0]=people[index[i]][0];
-            updated_arr[i][1]=people[index[i]][1];
-        }
-        return updated_arr;
+        return tempRes.toArray(new int[people.length][]);
     }
 }
