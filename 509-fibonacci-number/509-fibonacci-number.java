bd54b2e4b4c95d4e1ffe438338e9cba4328344d6
Udayan Banik <banikuday2708@gmail.com>
2022-07-06 06:19:30 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (28.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,6 +1,15 @@
 class Solution {
-    public int fib(int N) {
-        if( N < 2 ) return N;
-        return fib(N-1) + fib(N-2);
+    public int fib(int n) {
+        
+        int[] f = new int[n+2];
+        
+        f[0] = 0;
+        f[1] = 1;
+        for(int i =2; i<=n;i++){
+            f[i] = f[i-1]+f[i-2];
+        }
+        
+        return f[n];
+        
     }
 }
