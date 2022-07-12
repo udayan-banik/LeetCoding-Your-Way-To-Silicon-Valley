453aaa6894e2da3cb6bb8353455b500a396ecd05
Udayan Banik <banikuday2708@gmail.com>
2022-07-12 10:48:48 +0530

    Time: 3 ms (97.41%), Space: 41.8 MB (56.74%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,94 +1,34 @@
-import java.util.HashMap;
-
 class Solution {
-
-    // The memoization cache to be used during recursion.
-    public HashMap<Pair<Integer, Integer>, Boolean> memo;
-
-    // Array containing our matchsticks.
-    public int[] nums;
-
-    // Possible side of our square depending on the total sum of all the matchsticks. 
-    public int possibleSquareSide;
-
-    // Default constructor to initialise our memo cache.
-    public Solution() {
-        this.memo = new HashMap<Pair<Integer, Integer>, Boolean>();
-    }
-
-    // Main DP function.
-    public boolean recurse(Integer mask, Integer sidesDone) {
-        int total = 0;
-        int L = this.nums.length;
-
-        // The memo key for this recursion
-        Pair<Integer, Integer> memoKey = new Pair(mask, sidesDone);
-
-        // Find out the sum of matchsticks used till now.
-        for(int i = L - 1; i >= 0; i--) {
-            if ((mask&(1 << i)) == 0) {
-                total += this.nums[L - 1 - i];
-            }
-        }
-
-        // If the sum if divisible by our square's side, then we increment our number of complete sides formed variable.
-        if (total > 0 && total % this.possibleSquareSide == 0) {
-            sidesDone++;
-        }
-
-        // Base case.
-        if (sidesDone == 3) {
-            return true;
-        }
-
-
-        // Return precomputed results
-        if (this.memo.containsKey(memoKey)) {
-            return this.memo.get(memoKey);
-        }
-
-        boolean ans = false;
-        int c = total / this.possibleSquareSide;
-
-        // Remaining vlength in the current partially formed side.
-        int rem = this.possibleSquareSide * (c + 1) - total;
-
-        // Try out all remaining options (that are valid)
-        for(int i = L - 1; i >= 0; i--) {
-            if (this.nums[L - 1 - i] <= rem && (mask&(1 << i)) > 0) {
-                if (this.recurse(mask ^ (1 << i), sidesDone)) {
-                    ans = true;
-                    break;
-                }
-            }
-        }
-
-        // Cache the computed results.
-        this.memo.put(memoKey, ans);
-        return ans;
-    }
-
+    boolean[] st;
     public boolean makesquare(int[] nums) {
-
-        // Empty matchsticks.
-        if (nums == null || nums.length == 0) {
-            return false;
+        int sum = 0;
+        for(int x:nums) sum += x;
+        if(sum == 0 || sum % 4 != 0) return false;
+        Arrays.sort(nums);
+        int[] arr = new int[nums.length];
+        for(int i = 0;i < nums.length;i++) {
+            arr[i] = nums[nums.length -1 -i];
         }
-
-        // Find the perimeter of the square (if at all possible)
-        int L = nums.length;
-        int perimeter = 0;
-        for(int i = 0; i < L; i++) {
-            perimeter += nums[i];
+        st = new boolean[nums.length];
+        return dfs(nums,0,0,0,sum/4);
+    }
+    
+    boolean dfs(int[] nums,int u,int cur,int start,int length) {
+        if(u == 4) return true;
+        if(cur == length) return dfs(nums,u+1,0,0,length);
+        
+        for(int i =start;i < nums.length;i++) {
+            if(!st[i] && cur + nums[i] <= length) {
+                st[i] = true;
+                if(dfs(nums,u,cur + nums[i],i+1,length)) return true;
+                st[i] =false;
+                
+                
+                if(cur == 0) return false;
+                if(cur + nums[i] == length) return false;
+                while(i + 1 < nums.length && nums[i+1] == nums[i]) i++;
+            }
         }
-
-        int possibleSquareSide =  perimeter / 4;
-        if (possibleSquareSide * 4 != perimeter) {
-            return false;
-        }
-
-        this.nums = nums;
-        this.possibleSquareSide = possibleSquareSide;
-        return this.recurse((1 << L) - 1, 0);
+        return false;
     }
 }
