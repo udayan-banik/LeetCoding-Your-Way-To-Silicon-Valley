477a90a1acfda4caf9aded700dbe75cdda816c81
Udayan Banik <banikuday2708@gmail.com>
2022-07-30 11:34:27 +0530

    Time: 30 ms (77.64%), Space: 88.2 MB (60.16%) - LeetHub

--- 916-word-subsets/916-word-subsets.java
+++ 916-word-subsets/916-word-subsets.java
@@ -1,28 +1,35 @@
 class Solution {
-    public List<String> wordSubsets(String[] A, String[] B) {
-        int[] Bfreq = new int[26], check = new int[26];
-        int cmax = 0;
-        List<String> ans = new ArrayList<>();
-        for (int i = 0; i < B.length; i++, Arrays.fill(check, 0)) {
-            for (char c : B[i].toCharArray())
-                check[c - 'a']++;
-            for (int j = 0; j < 26; j++) {
-                int diff = check[j] - Bfreq[j];
-                if (diff > 0) {
-                    cmax += diff;
-                    Bfreq[j] += diff;
-                }
-            }
-            if (cmax > 10) return ans;
-        }
-        for (int i = 0; i < A.length; i++, Arrays.fill(check, 0)) {
-            int j;
-            for (char c : A[i].toCharArray())
-                check[c - 'a']++;
-            for (j = 0; j < 26; j++)
-                if (check[j] < Bfreq[j]) break;
-            if (j == 26) ans.add(A[i]);
-        }
-        return ans;
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+		List<String> result=new ArrayList<>();
+		int word2Count[] = new int[26];
+		
+		//Form word2 with array on count of each possible alphabets from words
+		for(String wordTwo: words2) {
+			int temp[] = new int[26];
+			for(char c:wordTwo.toCharArray()) {
+				temp[c-'a']++;
+				word2Count[c-'a'] = Math.max(word2Count[c-'a'], temp[c-'a']);
+			}
+		}
+
+		//Form word1 with array on count of each possible alphabets from words
+		for(String wordOne: words1) {
+			int word1Count[] = new int[26];
+			for(char c:wordOne.toCharArray()) {
+				word1Count[c-'a']++;
+			}
+			
+			boolean canAdd=true;
+			for(int i=0;i<26;i++) {
+				if(word1Count[i]<word2Count[i]) {
+					canAdd=false;
+					break;
+				}
+			}
+			if(canAdd)
+				result.add(wordOne);
+		}
+
+		return result;
     }
 }
