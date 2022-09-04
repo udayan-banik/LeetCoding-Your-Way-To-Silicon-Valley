8261164d261e066f288c2a7e5237c5b8d35e214b
Udayan Banik <banikuday2708@gmail.com>
2022-09-04 11:37:38 +0530

    Time: 3 ms (95.53%), Space: 42.6 MB (82.68%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -14,71 +14,54 @@
  * }
  */
 class Solution {
-  List < List < Integer >> verticalTraversal(TreeNode root) {
-
-    if (root == null) return Collections.emptyList();
-
-    List<List<Node>> posList = new ArrayList<>();
-    List<List<Node>> negList = new ArrayList<>();
-    dfs(root, 0, 0, negList, posList);
-    return mergeAndSort(negList, posList);
-  }
-
-  class Node {
-    int val;
-    int row;
     
-    public Node(int val, int row) {
-        this.val = val;
-        this.row = row;
-    }  
-  }
-
-  void dfs(TreeNode node, int row, int col, List<List<Node>> negList, List<List<Node>> posList) {
-
-    if (node == null) return;
-
-    if (col >= 0) {
-      if (posList.size() == col) {
-        posList.add(new ArrayList < >());
-      }
-      posList.get(col).add(new Node(node.val, row));
-
-    } else {
-      int posCol = -col - 1;
-      if (negList.size() == posCol) {
-        negList.add(new ArrayList<>());
-      }
-      negList.get(posCol).add(new Node(node.val, row));
-    }
-
-    // (row + 1, col - 1) left
-    dfs(node.left, row + 1, col - 1, negList, posList);
-
-    // and (row + 1, col + 1) right
-    dfs(node.right, row + 1, col + 1, negList, posList);
-
-  }
-
-  List<List<Integer>> mergeAndSort(List<List<Node>> negList, List<List<Node>> posList) {
-    List<List<Integer>> res = new ArrayList<>();
-
-    for (int i = negList.size() - 1; i >= 0; i--) {
-      List<Node> colList = negList.get(i);
-      colList.sort((a, b) -> a.row == b.row ? a.val - b.val : a.row - b.row);
-      res.add(buildList(colList)); 
-    }
-
-    for (int i = 0; i < posList.size(); i++) {
-      List<Node> colList = posList.get(i);
-      colList.sort((a, b) -> a.row == b.row ? a.val - b.val : a.row - b.row);
-      res.add(buildList(colList)); 
-    }
-
-    return res;
-  }
+    int maxL = 0;
     
-  List<Integer> buildList(List<Node> list) {
-      return list.stream().map(a -> a.val).collect(Collectors.toList());
-  }  
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<List<Integer>>> tree = new ArrayList();
+        dfs(root, 0, 0, tree);
+        List<List<Integer>> res = new ArrayList();
+        for (int i = 0; i < tree.size(); ++i) {
+            List<Integer> temp = new ArrayList();
+            for (List<Integer> row : tree.get(i))
+                for (int val : row)
+                    temp.add(val);
+            
+            res.add(temp);
+        }
+        
+        return res;
+    }
+    
+    void dfs(TreeNode node, int r, int c, List<List<List<Integer>>> res) {
+        if (node == null)
+            return;
+        
+        if (c < maxL)
+            res.add(0, new ArrayList());
+            
+        maxL = Math.min(maxL, c);
+        dfs(node.left, r+1, c-1, res);
+        
+        int col = c - maxL;
+        while (col >= res.size())
+            res.add(new ArrayList());
+        
+        int row = r / 2;
+        while (row >= res.get(col).size())
+            res.get(col).add(new ArrayList());
+        
+        int ind = getIndex(res.get(col).get(row), node.val);
+        res.get(col).get(row).add(ind, node.val);
+
+        dfs(node.right, r+1, c+1, res);
+    }
+    
+    int getIndex(List<Integer> list, int val) {
+        int ind = Collections.binarySearch(list, val);
+        if (ind < 0)
+            ind = Math.abs(ind) - 1;
+        
+        return ind;
+    }
 }
