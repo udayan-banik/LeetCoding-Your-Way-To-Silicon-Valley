ce2416638a7400ac6279fb42655cbd3a4e30e132
Udayan Banik <banikuday2708@gmail.com>
2022-09-09 09:42:28 +0530

    Time: 10 ms (93.41%), Space: 128.7 MB (81.67%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -1,35 +1,37 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.LinkedList;
-
 class Solution {
     public int numberOfWeakCharacters(int[][] properties) {
-        if (properties == null || properties.length < 2) {
-            return 0;
+        int maxAttack = 0;
+        // Find the maximum atack value
+        for (int[] property : properties) {
+            int attack = property[0];
+            maxAttack = Math.max(maxAttack, attack);
+        }
+        
+        int maxDefense[] = new int[maxAttack + 2];
+        // Store the maximum defense for an attack value
+        for (int[] property : properties) {
+            int attack = property[0];
+            int defense = property[1];
+            
+            maxDefense[attack] = Math.max(maxDefense[attack], defense);
         }
 
-        Arrays.sort(properties, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) {
-                    return Integer.compare(o2[1], o1[1]);
-                }
-                return Integer.compare(o1[0], o2[0]);
-            }
-        });
-
-        int max = 0;
-
-        int ans = 0;
-
-        for (int i = properties.length - 1; i >= 0; --i) {
-            if (properties[i][1] < max) {
-                ans++;
-            }
-            max = Math.max(properties[i][1], max);
+        // Store the maximum defense for attack greater than or equal to a value
+        for (int i = maxAttack - 1; i >= 0; i--) {
+            maxDefense[i] = Math.max(maxDefense[i], maxDefense[i + 1]);
         }
-
-        return ans;
+        
+        int weakCharacters = 0;
+        for (int[] property : properties) {
+            int attack = property[0];
+            int defense = property[1];
+            
+            // If their is a greater defense for properties with greater attack
+            if (defense < maxDefense[attack + 1]) {
+                weakCharacters++;
+            }
+        }
+        
+        return weakCharacters;
     }
 }
