61d4ac88fb0511e40ece274070686d65bf2e639c
Udayan Banik <banikuday2708@gmail.com>
2022-09-15 12:57:42 +0530

    Time: 6 ms (99.70%), Space: 56.7 MB (99.40%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,18 +1,42 @@
 class Solution {
-  public int[] findOriginalArray(int[] changed) {
-    List<Integer> ans = new ArrayList<>();
-    Queue<Integer> q = new ArrayDeque<>();
-
-    Arrays.sort(changed);
-
-    for (final int num : changed)
-      if (!q.isEmpty() && num == q.peek()) {
-        q.poll();
-      } else {
-        q.offer(num * 2);
-        ans.add(num);
-      }
-
-    return q.isEmpty() ? ans.stream().mapToInt(i -> i).toArray() : new int[] {};
-  }
-}
+    public int[] findOriginalArray(int[] changed) {
+        // It can't be doubled array, if the size is odd
+        if (changed.length % 2 == 1) {
+            return new int[0];
+        }
+        
+        int maxNum = 0;
+        // Find the max element in the array
+        for (int num : changed) {
+            maxNum = Math.max(maxNum, num);
+        }
+        
+        int[] freq = new int[2 * maxNum + 1];
+        // Store the frequency in the map
+        for (int num : changed) {
+            freq[num]++;
+        }
+        
+        int[] original = new int[changed.length / 2];
+        int index = 0;
+        for (int num = 0; num <= maxNum; num++) {
+            // If element exists
+            if (freq[num] > 0) {
+                freq[num]--;
+                
+                int twiceNum = num * 2;
+                if (freq[twiceNum] > 0) {
+                    // Pair up the elements, decrement the count
+                    freq[twiceNum]--;
+                    // Add the original number to answer
+                    original[index++] = num;
+                    num--;
+                } else {
+                    return new int[0];
+                }
+            }
+        }
+        
+        return original;
+    }
+};
