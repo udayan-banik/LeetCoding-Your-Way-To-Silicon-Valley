1e17dc02c3d827c800b86d4b523bb16156ca9208
Udayan Banik <banikuday2708@gmail.com>
2022-10-23 02:40:34 +0530

    Time: 4 ms (96.42%), Space: 43.9 MB (76.10%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,66 +1,15 @@
 class Solution {
     public String minWindow(String s, String t) {
-
-        if (s.length() == 0 || t.length() == 0) {
-            return "";
+        int[] map = new int[128];
+        for(char c : t.toCharArray()) map[c]++;
+        int counter=t.length(), begin=0, end=0, d=Integer.MAX_VALUE, head=0;
+        while(end<s.length()){
+            if(map[s.charAt(end++)]-->0) counter--; //in t
+            while(counter==0){ //valid
+                if(end-begin<d)  d=end-(head=begin);
+                if(map[s.charAt(begin++)]++==0) counter++;  //make it invalid
+            }  
         }
-
-        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
-
-        for (int i = 0; i < t.length(); i++) {
-            int count = dictT.getOrDefault(t.charAt(i), 0);
-            dictT.put(t.charAt(i), count + 1);
-        }
-
-        int required = dictT.size();
-
-        // Filter all the characters from s into a new list along with their index.
-        // The filtering criteria is that the character should be present in t.
-        List<Pair<Integer, Character>> filteredS = new ArrayList<Pair<Integer, Character>>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (dictT.containsKey(c)) {
-                filteredS.add(new Pair<Integer, Character>(i, c));
-            }
-        }
-
-        int l = 0, r = 0, formed = 0;
-        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
-        int[] ans = { -1, 0, 0 };
-
-        // Look for the characters only in the filtered list instead of entire s.
-        // This helps to reduce our search.
-        // Hence, we follow the sliding window approach on as small list.
-        while (r < filteredS.size()) {
-            char c = filteredS.get(r).getValue();
-            int count = windowCounts.getOrDefault(c, 0);
-            windowCounts.put(c, count + 1);
-
-            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-                formed++;
-            }
-
-            // Try and contract the window till the point where it ceases to be 'desirable'.
-            while (l <= r && formed == required) {
-                c = filteredS.get(l).getValue();
-
-                // Save the smallest window until now.
-                int end = filteredS.get(r).getKey();
-                int start = filteredS.get(l).getKey();
-                if (ans[0] == -1 || end - start + 1 < ans[0]) {
-                    ans[0] = end - start + 1;
-                    ans[1] = start;
-                    ans[2] = end;
-                }
-
-                windowCounts.put(c, windowCounts.get(c) - 1);
-                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-                    formed--;
-                }
-                l++;
-            }
-            r++;
-        }
-        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+        return d==Integer.MAX_VALUE? "":s.substring(head, head+d);
     }
 }
