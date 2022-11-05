218a81a20fd680bc11f6c9d7d9fdd95ed9e46db0
Udayan Banik <banikuday2708@gmail.com>
2022-11-05 11:31:49 +0530

    Time: 39 ms (98.20%), Space: 45.4 MB (39.51%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,55 +1,83 @@
-class TrieNode {
-  public TrieNode[] children = new TrieNode[26];
-  public String word;
-}
-
 class Solution {
-  public List<String> findWords(char[][] board, String[] words) {
-    for (final String word : words)
-      insert(word);
-
-    List<String> ans = new ArrayList<>();
-
-    for (int i = 0; i < board.length; ++i)
-      for (int j = 0; j < board[0].length; ++j)
-        dfs(board, i, j, root, ans);
-
-    return ans;
-  }
-
-  private TrieNode root = new TrieNode();
-
-  private void insert(final String word) {
-    TrieNode node = root;
-    for (final char c : word.toCharArray()) {
-      final int i = c - 'a';
-      if (node.children[i] == null)
-        node.children[i] = new TrieNode();
-      node = node.children[i];
+    private static final char HASH_TAG = '#';
+    private Tree root = new Tree();
+    
+    public List<String> findWords(char[][] board, String[] words) {
+        for (String word : words) {
+            Tree temp = root;
+            // Test case 60/63 
+            for (int i = word.length() - 1; i >= 0; i--) {
+                int idx = word.charAt(i) - 'a';
+                if (temp.val[idx] == null) {
+                    temp.val[idx] = new Tree();
+                    temp.val[idx].parent = temp;
+                    temp.val[idx].idx = idx;
+                }
+                temp = temp.val[idx];
+            }
+            temp.word = word;
+        }    
+        return searchWord(board);
     }
-    node.word = word;
-  }
-
-  private void dfs(char[][] board, int i, int j, TrieNode node, List<String> ans) {
-    if (i < 0 || i == board.length || j < 0 || j == board[0].length)
-      return;
-    if (board[i][j] == '*')
-      return;
-
-    final char c = board[i][j];
-    TrieNode child = node.children[c - 'a'];
-    if (child == null)
-      return;
-    if (child.word != null) {
-      ans.add(child.word);
-      child.word = null;
+    
+    private List<String> searchWord(char[][] board) {
+        List<String> res = new ArrayList<>();
+        
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[0].length; j++) {
+                Tree temp = root;
+                checkWord(res, i, j, board, temp);
+            }
+        
+        return res;
     }
+    
+    private void checkWord(List<String> res, int y, int x, char[][] board, Tree temp) {
+        
+        if (x >= board[0].length || x < 0
+            || y >= board.length || y < 0)
+            return;
+        
+        char ch = board[y][x];
+        if (ch== HASH_TAG || temp.val[ch - 'a'] == null)
+            return;
+        
+        temp = temp.val[ch - 'a'];
+        
+        if (temp.word != null) {
+            res.add(temp.word);
+            temp.word = null; // Test case 17/63: When there is more than one answer
+            Tree ptr = temp;
+            while (ptr.parent != null 
+                    && ptr.isEmpty()) {
+                int idx = ptr.idx;
+                ptr = ptr.parent;
+                ptr.val[idx] = null;
+            }
+        }
+        
+        board[y][x] = HASH_TAG;
 
-    board[i][j] = '*';
-    dfs(board, i + 1, j, child, ans);
-    dfs(board, i - 1, j, child, ans);
-    dfs(board, i, j + 1, child, ans);
-    dfs(board, i, j - 1, child, ans);
-    board[i][j] = c;
-  }
+        checkWord(res, y, x + 1, board, temp);
+        checkWord(res, y, x - 1, board, temp);
+        checkWord(res, y + 1, x, board, temp);
+        checkWord(res, y - 1, x, board, temp);
+        
+        board[y][x] = ch;
+    }
+    
+    class Tree {
+        Tree parent;
+        Tree[] val = new Tree[26];
+        String word;
+        int idx;
+
+        public boolean isEmpty() {
+            for (int i = 0; i < 26; i++) 
+                if (val[i] != null)
+                    return false;
+
+            return true;
+        }
+    }
 }
