a0d349712fc9004f26ac2bf5b82241d0c9ca07ba
Udayan Banik <banikuday2708@gmail.com>
2023-01-04 10:32:43 +0530

    Time: 53 ms (62.43%), Space: 93 MB (58.61%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,29 +1,20 @@
 class Solution {
     public int minimumRounds(int[] tasks) {
-        Map<Integer, Integer> freq = new HashMap();
-        // Store the frequencies in the map.
-        for (int task : tasks) {
-            freq.put(task, freq.getOrDefault(task, 0) + 1);
+        Arrays.sort(tasks);
+        int res = 0;
+        for (int i = 0; i < tasks.length;) {
+            int j = i + 1;
+            while (j < tasks.length && tasks[j] == tasks[i]) j++;
+            if (j == i + 1) return -1;
+            res += compute(j - i);
+            i = j;
         }
+        return res; 
+    }
 
-        int minimumRounds = 0;
-        // Iterate over the task's frequencies.
-        for (int count : freq.values()) {
-            // If the frequency is 1, it's not possible to complete tasks.
-            if (count == 1) {
-                return - 1;
-            }
-
-            if (count % 3 == 0) {
-                // Group all the task in triplets.
-                minimumRounds += count / 3;
-            } else {
-                // If count % 3 = 1; (count / 3 - 1) groups of triplets and 2 pairs.
-                // If count % 3 = 2; (count / 3) groups of triplets and 1 pair.
-                minimumRounds += count / 3 + 1;
-            }
-        }
-
-        return minimumRounds;
+    private int compute(int i) {
+        if (i % 3 == 0) return i / 3;
+        if (i % 3 == 2) return (i - 2) / 3 + 1;
+        return (i - 4) / 3 + 2;
     }
 }
