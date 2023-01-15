37d92879af81be042e22e687829ad7e49009edc1
Udayan Banik <banikuday2708@gmail.com>
2023-01-15 11:11:23 +0530

    Time: 32 ms (98.50%), Space: 62.9 MB (86.50%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.java
+++ 2421-number-of-good-paths/2421-number-of-good-paths.java
@@ -1,85 +1,40 @@
-class UnionFind {
-    int[] parent;
-    int[] rank;
-
-    public UnionFind(int size) {
-        parent = new int[size];
-        for (int i = 0; i < size; i++)
-            parent[i] = i;
-        rank = new int[size];
-    }
-
-    public int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    public void union_set(int x, int y) {
-        int xset = find(x), yset = find(y);
-        if (xset == yset) {
-            return;
-        } else if (rank[xset] < rank[yset]) {
-            parent[xset] = yset;
-        } else if (rank[xset] > rank[yset]) {
-            parent[yset] = xset;
-        } else {
-            parent[yset] = xset;
-            rank[xset]++;
-        }
-    }
-}
-
 class Solution {
     public int numberOfGoodPaths(int[] vals, int[][] edges) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        for (int[] edge : edges) {
-            int a = edge[0], b = edge[1];
-            adj.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
-            adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
-        }
-
         int n = vals.length;
-        // Mapping from value to all the nodes having the same value in sorted order of
-        // values.
-        TreeMap<Integer, List<Integer>> valuesToNodes = new TreeMap<>();
+        int res = n;
+        Arrays.sort(edges, (e1, e2) -> Integer.compare(Math.max(vals[e1[0]], vals[e1[1]]), Math.max(vals[e2[0]], vals[e2[1]])));
+        int[] group = new int[n];
+        int[] maxValue = new int[n];
+        int[] maxValueCount = new int[n];
         for (int i = 0; i < n; i++) {
-            valuesToNodes.computeIfAbsent(vals[i], value -> new ArrayList<Integer>()).add(i);
+            group[i] = i;
+            maxValue[i] = vals[i];
+            maxValueCount[i] = 1;
         }
 
-        UnionFind dsu = new UnionFind(n);
-        int goodPaths = 0;
-
-        // Iterate over all the nodes with the same value in sorted order, starting from
-        // the lowest value.
-        for (int value : valuesToNodes.keySet()) {
-            // For every node in nodes, combine the sets of the node and its neighbors into
-            // one set.
-            for (int node : valuesToNodes.get(value)) {
-                if (!adj.containsKey(node))
-                    continue;
-                for (int neighbor : adj.get(node)) {
-                    // Only choose neighbors with a smaller value, as there is no point in
-                    // traversing to other neighbors.
-                    if (vals[node] >= vals[neighbor]) {
-                        dsu.union_set(node, neighbor);
-                    }
+        for (int[] edge: edges) {
+            int g1 = getGroup(edge[0], group);
+            int g2 = getGroup(edge[1], group);
+            if (g1 != g2) {
+                group[g2] = g1;
+                if (maxValue[g1] < maxValue[g2]) {
+                    maxValueCount[g1] = 0;
+                } else if (maxValue[g1] > maxValue[g2]) {
+                    maxValueCount[g2] = 0;
                 }
-            }
-            // Map to compute the number of nodes under observation (with the same values)
-            // in each of the sets.
-            Map<Integer, Integer> group = new HashMap<>();
-            // Iterate over all the nodes. Get the set of each node and increase the count
-            // of the set by 1.
-            for (int u : valuesToNodes.get(value)) {
-                group.put(dsu.find(u), group.getOrDefault(dsu.find(u), 0) + 1);
-            }
-            // For each set of "size", add size * (size + 1) / 2 to the number of goodPaths.
-            for (int key : group.keySet()) {
-                int size = group.get(key);
-                goodPaths += size * (size + 1) / 2;
+                res += maxValueCount[g1] * maxValueCount[g2];
+                maxValue[g1] = Math.max(maxValue[g1], maxValue[g2]);
+                maxValueCount[g1] = maxValueCount[g1] + maxValueCount[g2];
             }
         }
-        return goodPaths;
+
+        return res;
+    }
+
+    private int getGroup(int node, int[] group) {
+        if (group[node] != node) {
+            group[node] = getGroup(group[node], group);
+        }
+        return group[node];
     }
 }
