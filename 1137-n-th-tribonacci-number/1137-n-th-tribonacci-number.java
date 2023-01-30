6dde1e84c439d0999df0445400915bae1ea87b7c
Udayan Banik <banikuday2708@gmail.com>
2023-01-30 09:55:15 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (98.67%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int tribonacci(int n) {
+         if (n <= 1) return n;
+            int t0 = 0;
+            int t1 = 1;
+            int t2 = 1;
+        for (int i = 3; i <= n; i++) {
+            int tmp = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tmp;
+        }
+        return t2;
+    }
+}
