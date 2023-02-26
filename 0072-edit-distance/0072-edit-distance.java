5c912db6a470bc28903af17dfb18edcd6f632b29
Udayan Banik <banikuday2708@gmail.com>
2023-02-26 09:33:49 +0530

    Time: 3 ms (99.97%), Space: 42.9 MB (23.02%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,37 +1,21 @@
 class Solution {
+    public int memo(String word1,String word2,int [][] dp,int i,int j){
+     if(i<0) return j+1;
+     if(j<0) return i+1;
+     if(dp[i][j]!=-1) return dp[i][j];
+     if(word1.charAt(i) == word2.charAt(j)){
+        return dp[i][j] = 0+ memo(word1,word2,dp,i-1,j-1);
+     }else{
+         return dp[i][j] = 1 + Math.min(memo(word1,word2,dp,i,j-1),Math.min(memo(word1,word2,dp,i-1,j),memo(word1,word2,dp,i-1,j-1)));
+     }
+    }
     public int minDistance(String word1, String word2) {
-        int word1Length = word1.length();
-        int word2Length = word2.length();
-
-        if (word1Length == 0) {
-            return word2Length;
+        int n = word1.length();
+        int m = word2.length(); 
+        int [][] dp = new int[n][m];
+        for(int [] rows:dp){
+            Arrays.fill(rows,-1);
         }
-        if (word2Length == 0) {
-            return word1Length;
-        }
-
-        int dp[][] = new int[word1Length + 1][word2Length + 1];
-
-        for (int word1Index = 1; word1Index <= word1Length; word1Index++) {
-            dp[word1Index][0] = word1Index;
-        }
-
-        for (int word2Index = 1; word2Index <= word2Length; word2Index++) {
-            dp[0][word2Index] = word2Index;
-        }
-
-        for (int word1Index = 1; word1Index <= word1Length; word1Index++) {
-            for (int word2Index = 1; word2Index <= word2Length; word2Index++) {
-                if (word2.charAt(word2Index - 1) == word1.charAt(word1Index - 1)) {
-                    dp[word1Index][word2Index] = dp[word1Index - 1][word2Index - 1];
-                } else {
-                    dp[word1Index][word2Index] = Math.min(dp[word1Index - 1][word2Index],
-                            Math.min(dp[word1Index][word2Index - 1],
-                                    dp[word1Index - 1][word2Index - 1])) + 1;
-                }
-            }
-        }
-
-        return dp[word1Length][word2Length];
+         return memo(word1,word2,dp,word1.length()-1,word2.length()-1);
     }
 }
