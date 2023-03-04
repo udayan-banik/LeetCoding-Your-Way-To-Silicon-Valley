ba30852446e9056024e2dbb8d0e1dc1795c56399
Udayan Banik <banikuday2708@gmail.com>
2023-03-04 21:45:58 +0530

    Time: 5 ms (99.69%), Space: 61 MB (18.89%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
@@ -1,26 +1,17 @@
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
-        // minPosition, maxPosition: the MOST RECENT positions of minK and maxK.
-        // leftBound: the MOST RECENT value outside the range [minK, maxK].
-        long answer = 0;
-        int minPosition = -1, maxPosition = -1, leftBound = -1;
-
-        // Iterate over nums, for each number at index i:
-        for (int i = 0; i < nums.length; ++i) {
-            // If the number is outside the range [minK, maxK], update the most recent leftBound.
-            if (nums[i] < minK || nums[i] > maxK)
-                leftBound = i;
-
-            // If the number is minK or maxK, update the most recent position.
-            if (nums[i] == minK)
-                minPosition = i;
-            if (nums[i] == maxK)
-                maxPosition = i;
-
-            // The number of valid subarrays equals the number of elements between leftBound and 
-            // the smaller of the two most recent positions (minPosition and maxPosition).
-            answer += Math.max(0, Math.min(maxPosition, minPosition) - leftBound);
+        long res = 0;
+            
+        int min=-1, max=-1, left = -1;
+        
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]<minK || nums[i] > maxK) {
+                left = min = max = i;
+            } 
+            min = nums[i]==minK ? i : min;
+            max = nums[i]==maxK ? i : max;
+            res += Math.min(max, min)-left;  
         }
-        return answer;
+        return res;
     }
 }
