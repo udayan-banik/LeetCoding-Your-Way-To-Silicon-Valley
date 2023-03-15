28e77b13bf5202122e333a056b20b8eafd4e8695
Udayan Banik <banikuday2708@gmail.com>
2023-03-15 09:56:11 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (67.59%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    public boolean dfs(TreeNode node, int index, int n) {
+        if (node == null) {
+            return true;
+        }
+        // If index assigned to current node is greater or equal to the number of nodes
+        // in tree, then the given tree is not a complete binary tree.
+        if (index >= n) {
+            return false;
+        }
+        // Recursively move to left and right subtrees.
+        return dfs(node.left, 2 * index + 1, n) &&
+                dfs(node.right, 2 * index + 2, n);
+    }
+
+    public boolean isCompleteTree(TreeNode root) {
+        return dfs(root, 0, countNodes(root));
+    }
+}
