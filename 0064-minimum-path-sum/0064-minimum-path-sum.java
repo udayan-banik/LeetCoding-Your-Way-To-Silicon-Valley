5ba8ceae90d8841d2a959e324f164136e85f81a5
Udayan Banik <banikuday2708@gmail.com>
2023-03-27 23:02:37 +0530

    Time: 0 ms (100.00%), Space: 46 MB (8.94%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,17 +1,29 @@
 class Solution {
-  public int minPathSum(int[][] grid) {
-    final int m = grid.length;
-    final int n = grid[0].length;
+    public int minPathSum(int[][] grid) {
+        helper(0,0,grid);
+        return grid[grid.length-1][grid[0].length-1];
+    }
 
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        if (i > 0 && j > 0)
-          grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
-        else if (i > 0)
-          grid[i][0] += grid[i - 1][0];
-        else if (j > 0)
-          grid[0][j] += grid[0][j - 1];
+    public static void helper(int r, int c, int grid[][]){
+        if(r>=grid.length || c>=grid[0].length)return ;
 
-    return grid[m - 1][n - 1];
-  }
+        if(r!=0 && c!=0){
+            grid[r][c]+= Math.min(grid[r-1][c], grid[r][c-1]);
+        }
+        for(int i=r+1; i<grid.length; i++){
+            if(c==0)
+            grid[i][c]+=grid[i-1][c];
+            else 
+            grid[i][c]+=Math.min(grid[i-1][c],grid[i][c-1]);
+        }
+
+        for(int i=c+1; i<grid[0].length; i++){
+            if(r==0)
+            grid[r][i]+=grid[r][i-1];
+            else 
+            grid[r][i]+=Math.min(grid[r][i-1],grid[r-1][i]);
+        }
+
+        helper(r+1,c+1,grid);
+    }
 }
