a639b06dd99dd645f874b7f88a98085e281a2691
Udayan Banik <banikuday2708@gmail.com>
2023-04-03 23:42:31 +0530

    Time: 17 ms (98.31%), Space: 50.7 MB (48.86%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,17 @@
+//read the solution again since not yet good enough
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int ans = 0;
+
+        while (i <= j) {
+            ans++;
+            if (people[i] + people[j] <= limit)
+                i++;
+            j--;
+        }
+
+        return ans;
+    }
+}
