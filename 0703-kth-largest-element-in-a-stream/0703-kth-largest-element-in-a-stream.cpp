4231adebe5482298a3bb0f42114431d2eb578626
Udayan Banik <banikuday2708@gmail.com>
2023-04-05 11:55:17 +0530

    Time: 30 ms (89.93%), Space: 19.9 MB (84.54%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,28 +1,37 @@
 class KthLargest {
-private:
-    int k;
-    priority_queue<int, vector<int>, greater<int>> n;
-    
 public:
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int ki;
     KthLargest(int k, vector<int>& nums) {
-        this->k=k;
-        
-        for(auto i:nums){
-            n.push(i);
-            if(n.size()>k) n.pop();
+        //int x= nums.size()-k+1;
+        ki=k;
+        int x= min(k,(int)nums.size());
+        for (int i=0; i<x; i++)pq.push(nums[i]);
+        for (int i=k; i<nums.size(); i++){
+            if (nums[i] > pq.top()){
+                pq.pop();
+                pq.push(nums[i]);
+            }
         }
-        
-        // sort(n.begin(),n.end(),greater<int>());
     }
     
     int add(int val) {
-        n.push(val);
-        
-        while (n.size() > k) n.pop();
-        
-        return n.top();
+        if (pq.size() < ki){
+            pq.push(val);
+            return pq.top();
+        }
+        if (val> pq.top()){
+            pq.pop();
+            pq.push(val);
+        }
+        return pq.top();
     }
 };
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
 
 /**
  * Your KthLargest object will be instantiated and called as such:
