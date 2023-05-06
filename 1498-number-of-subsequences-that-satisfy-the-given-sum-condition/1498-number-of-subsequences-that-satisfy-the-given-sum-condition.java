2bb8a3d1b2c743dc1980353a09b599fe0340b4c6
Udayan Banik <banikuday2708@gmail.com>
2023-05-06 18:42:10 +0530

    Time: 33 ms (100.00%), Space: 51.9 MB (15.76%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,31 +1,19 @@
-// two pointers approach
 
 class Solution {
-    public int numSubseq(int[] nums, int target) {
-        int n = nums.length;
-        int mod = 1_000_000_007;
-        Arrays.sort(nums);
-        
-        // Precompute the value of 2 to the power of each value.
-        int[] power = new int[n];
-        power[0] = 1;
-        for (int i = 1; i < n; ++i) {
-            power[i] = (power[i - 1] * 2) % mod;
-        }
-        
-        int answer = 0;
-        int left = 0, right = n - 1;
-
-        while (left <= right) {
-            if (nums[left] + nums[right] <= target) {
-                answer += power[right - left];
-                answer %= mod;
-                left++;
+        public int numSubseq(int[] A, int target) {
+        Arrays.sort(A);
+        int res = 0, n = A.length, l = 0, r = n - 1, mod = (int)1e9 + 7;
+        int[] pows = new int[n];
+        pows[0] = 1;
+        for (int i = 1 ; i < n ; ++i)
+            pows[i] = pows[i - 1] * 2 % mod;
+        while (l <= r) {
+            if (A[l] + A[r] > target) {
+                r--;
             } else {
-                right--;
+                res = (res + pows[r - l++]) % mod;
             }
         }
-        
-        return answer;
+        return res;
     }
 }
