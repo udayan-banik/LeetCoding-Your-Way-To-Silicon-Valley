ec5104381263e385f8416bb4e623302bb54c3565
Udayan Banik <banikuday2708@gmail.com>
2023-05-18 20:19:28 +0530

    Time: 11 ms (77.29%), Space: 90.5 MB (21.43%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -0,0 +1,20 @@
+// In-degree count
+class Solution {
+  public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+    // List to signify if the vertex has an incoming edge or not.
+    boolean[] isIncomingEdgeExists = new boolean[n];
+    for (List<Integer> edge : edges) {
+      isIncomingEdgeExists[edge.get(1)] = true;
+    }
+
+    List<Integer> requiredNodes = new ArrayList();
+    for (int i = 0; i < n; i++) {
+      // Store all the nodes that don't have an incoming edge.
+      if (!isIncomingEdgeExists[i]) {
+        requiredNodes.add(i);
+      }
+    }
+
+    return requiredNodes;
+  }
+}
