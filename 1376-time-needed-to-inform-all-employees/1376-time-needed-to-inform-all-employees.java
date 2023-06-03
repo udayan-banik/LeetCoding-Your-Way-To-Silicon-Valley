1fc3ce654e0379d7f01ae57fdfc0f0f4d3795485
Udayan Banik <banikuday2708@gmail.com>
2023-06-03 13:10:23 +0530

    Time: 10 ms (95.18%), Space: 57.3 MB (76.82%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,32 +1,20 @@
-//dfs
 class Solution {
-    int maxTime = Integer.MIN_VALUE;
-
-    void DFS(ArrayList<ArrayList<Integer>> adjList, int[] informTime, int curr, int time) {
-        // Maximum time for an employee to get the news.
-        maxTime = Math.max(maxTime, time);
-
-        for (int adjacent : adjList.get(curr)) {
-            // Visit the subordinate employee who gets the news after informTime[curr] unit time.
-            DFS(adjList, informTime, adjacent, time + informTime[curr]);
-        }
-    }
 
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<ArrayList<Integer>>(n);
-
-        for (int i = 0; i < n; i++) {
-            adjList.add(new ArrayList<Integer>());
-        }
-
-        // Making an adjacent list, each index stores the Ids of subordinate employees.
+        int result = 0;
         for (int i = 0; i < n; i++) {
             if (manager[i] != -1) {
-                adjList.get(manager[i]).add(i);
+                result = Math.max(result, check(manager, informTime, i));
             }
         }
+        return result;
+    }
 
-        DFS(adjList, informTime, headID, 0);
-        return maxTime;
+    public int check(int[] manager, int[] informTime, int source) {
+        if (manager[source] != -1) {
+            informTime[source] = informTime[source] + check(manager, informTime, manager[source]);
+        }
+        manager[source] = -1;
+        return informTime[source];
     }
 }
