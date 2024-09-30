4ce0e5b6fff07b006d67abe7da6dd5e1a68e9b0b
Udayan Banik <banikuday2708@gmail.com>
2024-09-30 19:59:04 +0530

    Time: 588 ms (25.66%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -3,7 +3,7 @@ SELECT s.student_id, s.student_name, sub.subject_name, COALESCE(e.attended_exams
 FROM Students s
 CROSS JOIN Subjects sub
 LEFT JOIN (
-    SELECT student_id, subject_name, COUNT(student_id) AS attended_exams
+    SELECT student_id, subject_name, COUNT(subject_name) AS attended_exams
     FROM Examinations
     GROUP BY student_id, subject_name
 ) e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
